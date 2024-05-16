7e254c7f9d25fd44d7635e749d021f9d39fe7d72
leyla <leylashd99@gmail.com>
2024-05-16 21:23:11 +0700

    Login UI

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const Login = () => {
   const router = useRouter();
+  const [message, setMessage] = useState("");
 
   async function handleLogin(formData) {
     const email = formData.get("email");
@@ -17,16 +19,49 @@ export const Login = () => {
     const data = await res.json();
     console.log(data);
 
+    setMessage(data.message);
+
     router.push("/dashboard");
   }
 
   return (
-    <main>
-      <form action={handleLogin}>
-        <input name="email" type="email" placeholder="email" />
-        <input name="password" type="password" placeholder="password" />
-        <button>Login</button>
+    <main className="space-y-6">
+      <section className="flex justify-center items-center w-full">
+        <h1 className="text-2xl">Welcome back</h1>
+      </section>
+      <form action={handleLogin} className="space-y-2">
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="input input-bordered input-success w-full"
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="input input-bordered input-success w-full"
+        />
+        <button className="btn btn-success w-full my-3">Login</button>
       </form>
+      {message !== "" ? (
+        <div role="alert" className="alert">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-info shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>{message}</span>
+        </div>
+      ) : null}
     </main>
   );
 };
